eb764f4840573f23c35909707c4ad7e03f9ec7c0
Vadim <eugene.s@zpoken.ai>
2022-10-11 15:21:43 +0300

    asd

--- babel-mocha.js
+++
@@ -1,4 +0,0 @@
-require("@babel/register")({
-    presets: [ "@babel/env" ],
-});
-require ("penumbra-web-assembly")
---
+++ test/wasm.test.ts
@@ -0,0 +1,14 @@
+import v from 'penumbra-web-assembly';
+
+var assert = require('assert');
+// import { generateSpendKey } from 'penumbra-web-assembly';
+const { getSeedPhrase } = require('../src/utils/getSeedPhrase');
+
+describe('Spend key', function() {
+  describe('generate spending key', function() {
+    it('should succesful generated spending key from seed', function() {
+      let seed = getSeedPhrase();
+      let spendKey = v.generateSpendKey(getSeedPhrase());
+    });
+  });
+});
