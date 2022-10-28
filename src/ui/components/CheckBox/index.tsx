058558136e70765cc2a560a1ffe9d23c940c674d
Zpoken <eugene.s@zpoken.ai>
2022-10-28 14:37:19 +0300

    Change checkbox

--- src/ui/components/CheckBox/index.tsx
+++ src/ui/components/CheckBox/index.tsx
@@ -6,10 +6,20 @@ export const CheckBox: React.FC<InputProps> = (props) => {
   return (
     <label className="flex items-center cursor-pointer">
       <input type="checkbox" className="checkbox" {...props} />
-      <div className="w-[30px] h-[30px] rounded-[5px] bg-gradient-to-r from-[rgba(139,228,217,0.6)] via-[rgba(200,184,128,0.6)] to-[rgba(255,144,47,0.5)] flex items-center justify-center">
-        <span className="cursor-pointer bg-brown rounded-[5px] w-[29px] h-[29px] flex items-center justify-center">
-          {props.checked ? <CheckSvg /> : ''}
-        </span>
+      <div
+        className={`flex items-center justify-center w-[30px] h-[30px] rounded-[5px] 
+     ${
+       props.checked
+         ? 'bg-gradient-to-r from-[rgba(139,228,217,0.6)] via-[rgba(200,184,128,0.6)] to-[rgba(255,144,47,0.5)]'
+         : 'input_default_border'
+     }
+      hover:bg-gradient-to-r hover:from-[rgba(139,228,217,0.6)] hover:via-[rgba(200,184,128,0.6)] hover:to-[rgba(255,144,47,0.5)]`}
+      >
+        <div className="flex items-center justify-center w-[29px] h-[29px]">
+          <span className="flex items-center justify-center cursor-pointer bg-brown rounded-[5px] w-[28px] h-[28px]">
+            {props.checked ? <CheckSvg /> : ''}
+          </span>
+        </div>
       </div>
       <span className="text_body text-light_grey ml-[8px]">{props.label}</span>
     </label>
