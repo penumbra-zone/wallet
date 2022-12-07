d1eb0cb08d9a039b2635bd1f83d1a02b2fdb1723
Valentine <valentine@zpoken.io>
2022-12-07 18:01:36 +0200

    [Valentine] CustodyProtocolService broadcast tx to tendermint

--- src/services/CustodyProtocolService.ts
+++ src/services/CustodyProtocolService.ts
@@ -5,6 +5,7 @@ import {
 } from "@buf/bufbuild_connect-web_penumbra-zone_penumbra/penumbra/core/transaction/v1alpha1/transaction_pb";
 import {sign_plan} from "penumbra-web-assembly";
 import {WalletController} from "../controllers";
+import {randomBytes, randomInt} from "crypto";
 
 export class CustodyProtocolService {
 
@@ -21,6 +22,25 @@ export class CustodyProtocolService {
         let authorizationData: AuthorizationData = sign_plan(this.getAccountSpendingKey(), plan);
 
         return authorizationData;
+    }
+
+    async broadcastTx(tx_bytes_hex: string) {
+
+        const broadcastResponse = await fetch('${tendermint}', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                "method": "broadcast_tx_sync",
+                "params": [tx_bytes_hex],
+                "id": randomInt(Number.MAX_SAFE_INTEGER),
+            })
+        });
+        const content = await broadcastResponse.json();
+
+        return content;
 
     }
 
