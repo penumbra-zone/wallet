c9d040e778154cdbf9273b461757f811ed572172
Tommi Niemi <tommi@niemi.lol>
2023-04-02 04:07:49 +0300

    add correct sizing to icons

--- icons/generate-icons.sh
+++ icons/generate-icons.sh
@@ -9,14 +9,14 @@ fi
 curl https://raw.githubusercontent.com/penumbra-zone/penumbra/main/docs/images/penumbra-dark.svg --output penumbra-logo.svg
 SVG_FILE="penumbra-logo.svg"
 
-# remove comments
+# remove comments to make black
 sed 's/<!--//g' $SVG_FILE >temp.svg
 sed -i 's/-->//g' temp.svg
 
 # Generate icons with black background using cairosvg
-cairosvg temp.svg -o icon16.png -W 16 -H 16
-cairosvg temp.svg -o icon48.png -W 48 -H 48
-cairosvg temp.svg -o icon128.png -W 128 -H 128
+cairosvg temp.svg -o icon16.png -W 16 -H 16 --output-width 16
+cairosvg temp.svg -o icon48.png -W 48 -H 48 --output-width 48
+cairosvg temp.svg -o icon128.png -W 128 -H 128 --output-width 128
 
 # Remove the temporary SVG file
 rm temp.svg
