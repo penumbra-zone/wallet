f6c15c31762682738e3e1a528f2b8ebf152523ea
Vadim <eugene.s@zpoken.ai>
2023-07-27 16:06:59 +0300

    update library

--- src/utils/wrapperPenumbraWasm.ts
+++ src/utils/wrapperPenumbraWasm.ts
@@ -1,8 +1,6 @@
 export let penumbraWasm
 
 if (process.env.NODE_GOAL === 'local') {
-	console.log(process.env.NODE_GOAL)
-
 	penumbraWasm = await require(process.env.WASM_ARTIFACTS_DIRECTORY_PATH)
 } else {
 	penumbraWasm = await require('penumbra-wasm')
